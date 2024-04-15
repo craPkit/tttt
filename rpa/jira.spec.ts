fb27a8a3e8ee07b320df944e078a76a4d2ae6aba
Patrick Nemenz <patrick.nemenz@adesso.at>
2024-04-15 11:51:48 +0200

    fix(jira): fix various errors and account for edge cases

--- rpa/jira.spec.ts
+++ rpa/jira.spec.ts
@@ -2,6 +2,10 @@
 
 import { expect, Page, test } from '@playwright/test';
 import fs from 'fs';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 type DataRow = [date: string, hours: string, desc: string];
 
@@ -11,18 +15,20 @@ test('read ozg components', async ({ page }) => {
   const content = fs.readFileSync('report.md').toString();
   const rows = parseContent(content);
 
-  await openJira(page, rows[0]);
+  await parseAndOpenJira(page, rows[0]);
   await loginJira(page);
 
   let lastIssue = '';
   const issueMap = new Map<string, string>();
 
   for (const row of rows) {
-    const {issueId, } = parseRow(row);
+    if (!row) break;
+
+    const { issueId, issuePrefix } = parseRow(row);
 
     if (lastIssue !== issueId) {
       lastIssue = issueId;
-      await openJira(page, row);
+      await openJira(page, issuePrefix, issueId);
     }
 
     const component = await page.locator('#components-field').textContent();
@@ -42,29 +48,24 @@ test('fill jira', async ({ page }) => {
   const content = fs.readFileSync('report.md').toString();
   const rows = parseContent(content);
 
-  await openJira(page, rows[0]);
+  await parseAndOpenJira(page, rows[0]);
   await loginJira(page);
 
   let lastIssue = '';
 
   for (const row of rows) {
-    const {
-      issue,
-      issuePrefix,
-      issueId,
-      date,
-      note,
-      jiraTimeString
-    } = parseRow(row);
+    if (!row) break;
+
+    const { issue, issuePrefix, issueId, date, note, jiraTimeString } = parseRow(row);
 
     if (lastIssue !== issueId) {
       lastIssue = issueId;
-      await openJira(page, row);
+      await openJira(page, issuePrefix, issueId);
     }
 
     if (
       !(await page.getByRole('menuitem', { name: 'Log work' }).isVisible({
-        timeout: 100,
+        timeout: 500,
       }))
     ) {
       const more = page.getByRole('button', { name: ' More' });
@@ -72,7 +73,9 @@ test('fill jira', async ({ page }) => {
       await more.click();
     }
 
-    await page.getByRole('menuitem', { name: 'Log work' }).click();
+    const startLogButton = page.getByRole('menuitem', { name: 'Log work' });
+    await expect(startLogButton).toBeVisible();
+    await startLogButton.click();
 
     const timeInput = page.locator('#log-work-time-logged');
     await expect(timeInput).toBeVisible();
@@ -93,7 +96,7 @@ test('fill jira', async ({ page }) => {
       }/${YYYY} 10:00`
     );
 
-    await noteInput.fill(note.slice(0, -1).trim());
+    await noteInput.fill(note);
 
     await page.getByRole('button', { name: 'Log' }).click();
 
@@ -106,28 +109,42 @@ test('fill jira', async ({ page }) => {
 function parseContent(content: string): DataRow[] {
   return content
     .split('\n')
-    .map((row) => row.split(','))
+    .map((row) => {
+      const parts = row.split(/^([\d.]+?),([\d.]+?),`(.+?)`$/);
+      parts.shift();
+      parts.pop();
+      return parts;
+    })
     .filter((row) => row.length === 3)
     .sort(([, , aDesc], [, , bDesc]) => aDesc.localeCompare(bDesc)) as DataRow[];
 }
 
 function parseRow(row: DataRow) {
   const [date, hours, description] = row;
-  const [issue, note] = description?.split(':');
-  const [issuePrefix, issueId] = issue.split(/[ -]/);
+  const [issue, note] = description?.split(':').map(trimDelimiters);
+  const [issuePrefix, issueId] = issue.split(/[ -]/).map(trimDelimiters);
 
   const [h, hourFractions] = hours.split('.');
-  // console.log(hours, h, dm);
   const jiraTimeString = `${h}h ${parseFloat(`0.${hourFractions}`) * 60}m`;
 
   return {
-    date, issue, note, issuePrefix, issueId, jiraTimeString
-  }
+    date,
+    issue,
+    note,
+    issuePrefix,
+    issueId,
+    jiraTimeString,
+  };
 }
 
-function openJira(page: Page, row: DataRow) {
-  const {issuePrefix} = parseRow(row);
-  return page.goto(`${process.env.JIRA_URL}/tasks/browse/${process.env.JIRA_PREFIX}${issuePrefix}-1`);
+function parseAndOpenJira(page: Page, row: DataRow) {
+  if (!row) throw new Error('data is malformed');
+  const { issuePrefix, issueId } = parseRow(row);
+  return openJira(page, issuePrefix, issueId);
+}
+
+function openJira(page: Page, issuePrefix: string, issueId: string) {
+  return page.goto(`${process.env.JIRA_URL}/tasks/browse/${issuePrefix}-${issueId ?? 1}`);
 }
 
 async function loginJira(page: Page) {
@@ -141,4 +158,8 @@ async function loginJira(page: Page) {
   await name.fill(process.env.JIRA_USER);
   await pass.fill(process.env.JIRA_PASS);
   await button.click();
+}
+
+function trimDelimiters(str: string) {
+  return [...str].filter((char) => char !== '`').join('');
 }
